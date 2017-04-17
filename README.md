4d122e7f3be5d75c4ef1b46546bf09001c31da6e
ZiBin <ZiBin_1688@126.com>
2017-04-18 01:03:43 +0800

    阅读视图添加载入动画

--- README.md
+++ README.md
@@ -9,5 +9,9 @@
        2.把启动图放到导航控制器上，修复启动图随着
          UITableViewController移动的问题
        3.添加阅读视图页面，下滑改变状态栏颜色功能
+       4.修改首页UITableViewController为
+         UIViewController,修复启动时自动出现下拉头视图 
+       5.点击滚动图添加阅读视图
        
-           
+### * 2017.4.18
+       1.阅读视图界面添加载入动画（无网络时有点bug）
--- ZBQDaily/ZBQDaily/Classes/ReaderView(阅读视图)/Controller/ZBReaderViewController.m
+++ ZBQDaily/ZBQDaily/Classes/ReaderView(阅读视图)/Controller/ZBReaderViewController.m
@@ -8,13 +8,18 @@
 
 #import "ZBReaderViewController.h"
 #import <WebKit/WebKit.h>
-
+#import "MBProgressHUD+MJ.h"
 
 
 @interface ZBReaderViewController ()<UIScrollViewDelegate,WKNavigationDelegate>
 {
     CGFloat _contentOffSet_Y; //WKWebView滑动后Y轴偏移量
 }
+
+/** 加载动画view*/
+@property (nonatomic, strong) UIView *loadingView;
+@property (nonatomic, strong) UIImageView *loadingImageView;
+
 @property (nonatomic,weak) WKWebView *readerWebView;
 //判断滑动方向
 @property (nonatomic,assign) BOOL iSDownDrag;
@@ -22,6 +27,33 @@
 @end
 
 @implementation ZBReaderViewController
+
+#pragma mark -- 懒加载
+- (UIView *)loadingView{
+    if (!_loadingView) {
+        _loadingView = [[UIView alloc] initWithFrame:[UIScreen mainScreen].bounds];
+        _loadingView.backgroundColor = [UIColor clearColor];
+    }
+    return _loadingView;
+}
+
+-(UIImageView *)loadingImageView{
+    if (!_loadingImageView) {
+        _loadingImageView = [[UIImageView alloc] init];
+        _loadingImageView.frame = CGRectMake(0, 0, 100, 100);
+        _loadingImageView.center = self.loadingView.center;
+        NSMutableArray *imageMutableArray = [NSMutableArray array];
+        for (int i = 0; i < 93; i++) {
+            UIImage *image = [UIImage imageNamed:[NSString stringWithFormat:@"QDArticleLoading_0%d",i]];
+            [imageMutableArray addObject:image];
+        }
+        _loadingImageView.animationImages = imageMutableArray;
+        _loadingImageView.animationDuration = 3.0;
+        _loadingImageView.animationRepeatCount = MAXFLOAT;
+    }
+    return _loadingImageView;
+}
+
 
 #pragma mark -- 视图生命周期
 - (void)viewDidLoad {
@@ -40,7 +72,7 @@
     [self addWebView];
 }
 
-#pragma mark -- WKWebView
+#pragma mark -- WKNavigationDelegate
 //添加WKWebView
 - (void)addWebView{
     WKWebView *readerWebView = [[WKWebView alloc] initWithFrame:CGRectMake(0, -20, ZBSCREEN_WIDTH, ZBSCREENH_HEIGHT + 20)];
@@ -49,12 +81,43 @@
     [readerWebView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:_HtmlUrl]]];
     _readerWebView = readerWebView;
     [self.view addSubview:readerWebView];
+    [self.view addSubview:self.loadingView];
+    [self.loadingView addSubview:self.loadingImageView];
 }
-#pragma mark -- UIScrollViewDelegate
-///// 停止滚动时调用
-//- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView {
-//    NSLog(@"%f", scrollView.contentOffset.y);
+/// WXWebView开始加载时调用
+- (void)webView:(WKWebView *)webView didStartProvisionalNavigation:(WKNavigation *)navigation {
+    [self.loadingImageView startAnimating];
+}
+/// WXWebView加载完成时调用
+- (void)webView:(WKWebView *)webView didFinishNavigation:(WKNavigation *)navigation {
+    //渐隐加载动画
+    [UIView animateWithDuration:0.3
+                     animations:^{
+                         [self.loadingView setAlpha:0];
+                     } completion:^(BOOL finished) {
+                         [self.loadingImageView stopAnimating];
+                         [self.loadingView removeFromSuperview];
+                         self.loadingView = nil;
+                     }];
+}
+/// WXWebView加载失败时调用
+//- (void)webView:(WKWebView *)webView didFailNavigation:(WKNavigation *)navigation withError:(NSError *)error {
+//    [self.loadingImageView stopAnimating];
+//    NSLog(@"2222");
+//    [MBProgressHUD showError:@"网络连接失败，请检查网络"];
+//    [NSThread sleepForTimeInterval:1.3];
+//    [self.navigationController popViewControllerAnimated:YES];
 //}
+/// WXWebView加载失败时调用
+- (void)webView:(WKWebView *)webView didFailProvisionalNavigation:(null_unspecified WKNavigation *)navigation withError:(NSError *)error{
+    NSLog(@"1111");
+    [self.loadingImageView stopAnimating];
+    [MBProgressHUD showError:@"网络连接失败，请检查网络"];
+    [NSThread sleepForTimeInterval:3];
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
+#pragma mark -- UIScrollViewDelegate
 - (void)scrollViewDidScroll:(UIScrollView *)scrollView{
     //判断滑动方向 正为向下滑动 负为向上滑动
     if (scrollView.contentOffset.y > _contentOffSet_Y) {
@@ -69,6 +132,7 @@
         [self setNeedsStatusBarAppearanceUpdate];
     }
 }
+
 #pragma mark -- Other
 //隐藏导航栏
 - (BOOL)fd_prefersNavigationBarHidden {
